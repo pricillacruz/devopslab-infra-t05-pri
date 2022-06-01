1c145aeb3beb9ba7bc1618970d011df381308d0a
Pricilla Souza Cruz <61021558+pricillacruz@users.noreply.github.com>
2022-05-31 23:22:17 -0300

    tirando espaco

--- providers.tf
+++ providers.tf
@@ -9,8 +9,7 @@ terraform {
 
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
-
-  project = "devopslab-infra-t05-pri"
+  project = "teste-351601"
   region  = "us-west1"
   zone    = "us-west1-b"
 }
