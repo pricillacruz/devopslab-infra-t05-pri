0d9a46f3320b172579d21924ff3dd6771fa76a87
Pricilla Souza Cruz <61021558+pricillacruz@users.noreply.github.com>
2022-05-31 23:49:55 -0300

    docker

--- providers.tf
+++ providers.tf
@@ -2,14 +2,20 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "4.22.0"
+      version = "4.12.0"
     }
   }
 }
 
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
-  project = "teste-351601"
+  project = "devopslab-infra-t05-pri"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
+
+provider "google-beta" {
+  project = "devopslab-infra-t05-pri"
   region  = "us-west1"
   zone    = "us-west1-b"
 }
